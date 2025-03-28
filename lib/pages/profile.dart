c6f695d38c7faab024b3912ae87ba6888ab4d9c2
domiedosa1977 <edosadominion25@gmail.com>
2025-03-28 15:39:12 +0100

    i added a container for profile

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -5,14 +5,28 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: 300,
-        height: 200,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(40),
-        ),
-        child: Image.asset("images\tomato-sauce-jar-2 1.png",fit: BoxFit.contain,),
+      body: Column(
+        children: [
+          Container(
+            width: 300,
+            height: 200,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(40),
+            ),
+            child: Image.asset("images/tomato-sauce-jar-2 1.png",fit: BoxFit.contain,),
+          ),
+          SizedBox(height: 20,),
+          Container(
+            width: 200,
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.black12,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child:Icon(Icons.person),
+          )
+        ],
       ),
     );
   }
